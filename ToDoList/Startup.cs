6bd0218cabeec28d9623df1d61ed6e81de3741a9
Student <example@example.com>
2019-10-23 13:55:28 -0700

    add authentication

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoList.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace ToDoList
 {
@@ -25,8 +26,12 @@ namespace ToDoList
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<ToDoListContext>(options => options
-        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+          .AddDbContext<ToDoListContext>(options => options
+          .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+      
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+          .AddEntityFrameworkStores<ToDoListContext>()
+          .AddDefaultTokenProviders();
     }
 
     public void Configure(IApplicationBuilder app)
@@ -34,6 +39,8 @@ namespace ToDoList
       app.UseStaticFiles();
 
       app.UseDeveloperExceptionPage();
+
+      app.UseAuthentication();
 
       app.UseMvc(routes =>
       {
